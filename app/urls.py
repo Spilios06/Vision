22d0a370bc770adc4752087fcd020b99ec3b82a5
Spilios06 <smaderakis06@gmail.com>
2023-01-16 11:03:43 +0200

    Sign-Up functionality

--- app/urls.py
+++ app/urls.py
@@ -4,3 +4,7 @@ from django.conf.urls.static import static
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, 
 document_root=settings.STATIC_ROOT)
+
+urlpatterns = [
+    path('signup/', signup, name='signup'),
+]
