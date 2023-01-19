6731e5747a3ed84635e86db41e2a9cef07b2ca7c
Spilios06 <smaderakis06@gmail.com>
2023-01-19 15:21:37 +0200

    Finally got the media directory working

--- app/settings.py
+++ app/settings.py
@@ -121,8 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = '/Vision/Images'
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'Images')
 MEDIA_URL = '/Images/'
 
 # Default primary key field type
