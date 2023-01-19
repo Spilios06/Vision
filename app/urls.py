be562e167bbaec26eccf68be4923c135adb96ed2
Spilios06 <smaderakis06@gmail.com>
2023-01-19 18:51:40 +0200

    Fixed login button functionality

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from .views import index
+from .views import login
 
 urlpatterns = [
     #path('signup/', signup, name='signup'),
     path('', index, name='index'),
+    path('login/', login, name='login')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
